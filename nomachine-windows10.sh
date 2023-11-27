f652cd0b55bf2f098c83985b8711920a44aa5ca3
Nabin_sharma <105613345+NabinSharmaitgithub@users.noreply.github.com>
2023-11-27 06:24:54 +0545

    Add files via upload

---
+++ nomachine-windows10.sh
@@ -0,0 +1,126 @@
+echo ""
+printf "\e[100;330m[\e[10m **** ]\e[1;40m\e[10m :\e[1;32m Join Telegram Channel \e[1;33m  !\e[0m"
+sleep 4
+echo ""
+clear
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[92m This Cloud Linux RDP working only NoMachine VNC Application and Software you Download NoMachine Application and Connect this RDP\e[m "
+echo ""
+echo "Download NoMachine VNC App : https://www.nomachine.com/download"
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[93m Click Enter to Continue   : \e[0m' option
+echo""
+echo""
+clear
+
+wget -O ng.sh https://raw.githubusercontent.com/NabinSharmaitgithub/Window-10-RDP/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+echo ""
+echo -e "\e[92m 
+ ███▄    █   ▄████  ██▀███   ▒█████   ██ ▄█▀
+ ██ ▀█   █  ██▒ ▀█▒▓██ ▒ ██▒▒██▒  ██▒ ██▄█▒ 
+▓██  ▀█ ██▒▒██░▄▄▄░▓██ ░▄█ ▒▒██░  ██▒▓███▄░ 
+▓██▒  ▐▌██▒░▓█  ██▓▒██▀▀█▄  ▒██   ██░▓██ █▄ 
+▒██░   ▓██░░▒▓███▀▒░██▓ ▒██▒░ ████▓▒░▒██▒ █▄
+░ ▒░   ▒ ▒  ░▒   ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+░ ░░   ░ ▒░  ░   ░   ░▒ ░ ▒░  ░ ▒ ▒░ ░ ░▒ ▒░
+   ░   ░ ░ ░ ░   ░   ░░   ░ ░ ░ ░ ▒  ░ ░░ ░ 
+         ░       ░    ░         ░ ░  ░  ░   
+\e[0m\n"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo""
+echo -e "\e[31m[\e[32m*\e[31m]\e[93m Go to : \e[97m https://dashboard.ngrok.com/get-started/your-authtoken \e[m "
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[93m Sign up & get ngrok authtoken \e[m "
+echo ""
+read -p $'\e[31m[\e[32m*\e[31m]\e[93m Paste Ngrok Authtoken: \e[96m [Ex. 2cEGBt**********WK5Ntc ] : \e[0m' CRP
+echo ""
+./ngrok authtoken $CRP 
+
+clear
+echo ""
+echo ""
+echo -e " \e[36mWebsite : \e[0m\e[92mhttps://termuxbynabinsharma.blogspot.com/2023/11/window-10-rdp.html \e[93m/  \e[92mhttps://github.com/NabinSharmaitgithub/Window-10-RDP  \e[0m\n"
+echo ""
+echo -e " \e[97mTelegram : \e[0m\e[93m \e[0m\n"
+echo ""
+echo "Repo: https://github.com/NabinSharmaitgithub/Window-10-RDP"
+echo ""
+echo "==========================================================="
+echo -e "\e[96mChoose Ngrok Region (for better connection).\e[0m"
+echo "==========================================================="
+echo -e "us - \e[93mUnited States \e[92m(Ohio)\e[0m"
+echo -e "eu - \e[93mEurope \e[92m(Frankfurt)\e[0m"
+echo -e "ap - \e[93mAsia/Pacific \e[92m(Singapore)\e[0m"
+echo -e "au - \e[93mAustralia \e[92m(Sydney)\e[0m"
+echo -e "sa - \e[93mSouth America \e[92m(Sao Paulo)\e[0m"
+echo -e "jp - \e[93mJapan \e[92m(Tokyo)\e[0m"
+echo -e "in - \e[93mIndia \e[92m(Mumbai)\e[0m"
+echo ""
+read -p "Choose Ngrok Region: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+sleep 1
+clear
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Download Cloud Linux RDP Tool Debian - Windows 10 Theme Desktop Environmen \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   -----------------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;93m  !!      Download Ubuntu Windows 10 Theme        !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ------------------------------------------------ \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
+echo ""
+sleep 1
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=nabin -e USER=nabin --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+clear
+echo -e "\e[92m 
+███╗   ██╗ █████╗ ██████╗ ██╗███╗   ██╗    
+████╗  ██║██╔══██╗██╔══██╗██║████╗  ██║    
+██╔██╗ ██║███████║██████╔╝██║██╔██╗ ██║    
+██║╚██╗██║██╔══██║██╔══██╗██║██║╚██╗██║    
+██║ ╚████║██║  ██║██████╔╝██║██║ ╚████║    
+╚═╝  ╚═══╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝    
+███████╗██╗  ██╗ █████╗ ██████╗ ███╗   ███╗ █████╗ 
+██╔════╝██║  ██║██╔══██╗██╔══██╗████╗ ████║██╔══██╗
+███████╗███████║███████║██████╔╝██╔████╔██║███████║
+╚════██║██╔══██║██╔══██║██╔══██╗██║╚██╔╝██║██╔══██║
+███████║██║  ██║██║  ██║██║  ██║██║ ╚═╝ ██║██║  ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝  \e[95;1m \e[0m\n"
+  echo""
+sleep 1
+echo -e "\e[96mWebsite : \e[93mhttps://termuxbynabinsharma.blogspot.com/2023/11/window-10-rdp.html \e[0m"
+echo -----------------------------------------------------------------
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[92m Open NoMachine VNC Application and Software (Android/Windows). Connect this RDP. Enter IP Address ans Port and Click Connect RDP "
+echo ""
+echo -e ""
+echo -e "\e[93m IP Address:\e[97m" 
+  curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo -e "\e[96m User: \e[97m nabin"
+echo -e "\e[92m Password: \e[97m nabin"
+echo -----------------------------------------------------------------
+rm -rf Docker-Ubuntu/nomachine-windows10.sh
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
